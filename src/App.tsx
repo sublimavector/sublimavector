2e382d927411d3f4bce80bd2b57c80c2d6c40b6c
sublimavector <sublimavectorpersonalizados@gmail.com>
2025-06-08 19:26:06 -0300

    Teste de deploy CI/CD 05

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,8 @@ export function App() {
    
   return (
     <div className="flex w-full h-screen justify-center items-center bg-slate-200">
-      <h1 className="text-sky-600 font-bold text-3xl">SUBLIMAVECTOR-2</h1>
+      <h1 className="text-sky-600 font-bold text-3xl">SUBLIMAVECTOR</h1>
+      <h2>Deploy com sucesso CI/CD</h2>
     </div>
   );
 }
