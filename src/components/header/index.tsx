a6c6619811a107bd7c713cec7a0b99ad21ea07bb
sublimavector <sublimavectorpersonalizados@gmail.com>
2025-06-09 18:50:55 -0300

    Primeiro teste de navegação pelo menu

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,8 +1,22 @@
+import { Link } from 'react-router-dom';
+import logo from '../../assets/logo-sublimavector.svg'
+import { FiUser } from 'react-icons/fi';
+
 export function Header() {
+  const signed = false;
+  const loadingAuth = false;
 
   return (
     <div>
-      <h1>Header</h1>
+      <header>
+        <Link to="/">
+          <img src={logo} alt="Logo sublimavector" />
+        </Link>
+
+        <Link to="/dashboard">
+          <FiUser size={24} color="#000" />
+        </Link>
+      </header>
     </div>
   )
 }
